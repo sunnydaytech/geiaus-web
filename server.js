ce93e09e6cccb611c0b677a312df3e6794c997e6
Nicholas <nicholas.zhaoyu@gmail.com>
2016-05-24 22:34:02 -0700

    basic template

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var app = express();
 
+// Configure template
+var exphbs = require('express-handlebars');
+app.engine('.hbs', exphbs({defaultLayout: 'default', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
 app.use('/static', express.static(__dirname + '/public'));
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.render('index');
 });
+
+app.get('/signin', function(req, res){
+  res.render('signin');
+});
+
+app.get('/signup', function(req, res){
+  res.render('signup');
+});
+
+
 
 app.listen(8080);
